2fd982a6ecf152230c304959b0c2e56ebd617327
yangwei <2979026903@qq.com>
2020-08-20 15:16:53 +0800

    chajian8yue20

--- OnePos/src/main/java/com/yw/text/Onedemo.java
+++ OnePos/src/main/java/com/yw/text/Onedemo.java
@@ -1,20 +1,38 @@
 package com.yw.text;
 
 import kd.bos.bill.AbstractBillPlugIn;
-import kd.bos.form.control.events.*;
-
-import java.util.EventObject;
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.entity.datamodel.IDataModel;
+import kd.bos.entity.operate.result.OperationResult;
+import kd.bos.form.events.AfterDoOperationEventArgs;
+import kd.bos.form.events.HyperLinkClickEvent;
+import kd.fi.gl.constant.Voucher;
 
 
 public class Onedemo extends AbstractBillPlugIn {
 
-    /**
-     * 控件点击事件触发，通过Key判断触发来源执行不同操作。
+    /*
+     *超链接点击
      */
-    @Override
-    public void itemClick(ItemClickEvent evt) {
-        super.itemClick(evt);
-            this.getView().showMessage("save");
+    public void hyperLinkClick(HyperLinkClickEvent evt) {
+        String key = evt.getFieldName();
+        if (Voucher.ACCT.equals(key)) {
+            IDataModel model = this.getModel();
+            //获取操作行索引
+            int rowIndex = evt.getRowIndex();
+            DynamicObject voucher = model.getDataEntity(true);
+            //获取指定行单据体字段值
+            long acctId = (long) model.getValue(Voucher.id_(Voucher.ACCT), rowIndex);
+            long curId = (long) model.getValue(Voucher.id_(Voucher.CURRENCY), rowIndex);
+            //do hyperLink operate
+        }
+    }
+    public void afterDoOperation(AfterDoOperationEventArgs evt) {
+        String name = evt.getOperateKey();
+        OperationResult operationResult = evt.getOperationResult();
+        if (name.equals("ftextfield5") && operationResult.isSuccess()) {
+            this.getView().setVisible(false, "creator");
+        }
     }
 
 }
--- OnePos/src/main/java/com/yw/text/TwoCJ.java
+++ OnePos/src/main/java/com/yw/text/TwoCJ.java
@@ -1,11 +1,17 @@
 package com.yw.text;
 
 import kd.bos.form.FormShowParameter;
+import kd.bos.form.control.Button;
+import kd.bos.form.control.Control;
+import kd.bos.form.control.events.ClickListener;
 import kd.bos.form.events.PreOpenFormEventArgs;
 import kd.bos.form.plugin.AbstractFormPlugin;
 import kd.bos.servicehelper.model.PermissionStatus;
 
-public class TwoCJ extends AbstractFormPlugin {
+import java.util.EventObject;
+
+public class TwoCJ extends AbstractFormPlugin  implements ClickListener {
+
     @Override
     public void preOpenForm(PreOpenFormEventArgs e) {
 
@@ -14,6 +20,24 @@ public class TwoCJ extends AbstractFormPlugin {
         showParameter.setListentimerElapsed(true);
 
         // 设置显示参数 - 界面标题
-        showParameter.setCaption("hello world");
+        showParameter.setCaption("人员申请单");
     }
+
+        public void regiterListener(EventObject evt){
+            // 1.注册监听
+            Button btn = this.getView().getControl("按钮标识");
+            btn.addClickListener(this);
+        }
+
+        public void click(EventObject evt) {
+            // 2. 响应方法，do something...
+            Control control = (Control) evt.getSource();
+            String key = control.getKey();
+            if ("按钮标识".equals(key)) {
+                // do some cool thing...
+                this.getView().showMessage("1111111111111111111");
+            }
+    }
+
+
 }
